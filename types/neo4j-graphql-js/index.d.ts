c6108729a58e7dd41393c61ce6e0977a836af258
Lars Krieger <lars.krieger@gmail.com>
2020-06-03 09:59:22 +0200

    add better js doc for each of the exported functions in the module

--- types/neo4j-graphql-js/index.d.ts
+++ types/neo4j-graphql-js/index.d.ts
@@ -7,7 +7,25 @@
 declare module 'neo4j-graphql-js' {
     import { GraphQLSchema, GraphQLFieldResolver, GraphQLResolveInfo, ExecutionResult } from 'graphql';
 
+    /**
+     * makeAugmentSchema
+     * @description Wraps {@link https://www.apollographql.com/docs/apollo-server/api/apollo-server/#makeExecutableSchema|makeExecutableSchema} to create a GraphQL schema from GraphQL type definitions (SDL). Will generate Query and Mutation types for the provided type definitions and attach `neo4jgraphql` as the resolver for these queries and mutations. Either a schema or typeDefs must be provided. `resolvers` can optionally be implemented to override any of the generated Query/Mutation fields. Additional options are passed through to `makeExecutableSchema`.
+     *
+     * @param {MakeAugmentSchemaOptions} options
+     */
     export function makeAugmentSchema(options: MakeAugmentSchemaOptions): GraphQLSchema;
+
+    /**
+     * neo4jgraphql
+     * @description This function's signature matches that of {@link https://graphql.org/learn/execution/#root-fields-resolvers|GraphQL resolver functions}. and thus the parameters match the parameters passed into resolve by GraphQL implementations like graphql-js.
+     *
+     * It can be called within a resolver to generate a Cypher query and handle the database call to Neo4j to completely resolve the GraphQL request. Alternatively, use `cypherQuery` or `cypherMutation` within a resolver to only generate the Cypher query and handle the database call yourself.
+     * @param {object} object                   The previous object being resolved. Rarely used for a field on the root Query type.
+     * @param {RequestArguments} args           The arguments provided to the field in the GraphQL query.
+     * @param {object} context                  Value provided to every resolver and hold contextual information about the request, such as the currently logged in user, or access to a database. neo4j-graphql-js assumes a neo4j-javascript-driver instance exists in this object, under the key driver.
+     * @param {GraphQLResolveInfo} resolveInfo  Holds field-specific infomation relevant to the current query as well as the GraphQL schema.
+     * @param {boolean} debug                   Specifies whether to log the generated Cypher queries for each GraphQL request. Logging is enabled by default.
+     */
     export function neo4jgraphql(
         object: any,
         args: RequestArguments,
@@ -15,8 +33,35 @@ declare module 'neo4j-graphql-js' {
         resolveInfo: GraphQLResolveInfo,
         debug: boolean,
     ): ExecutionResult;
+
+    /**
+     * augmentSchema
+     * @description Takes an existing GraphQL schema object and adds neo4j-graphql-js specific enhancements, including auto-generated mutations and queries, and ordering and pagination fields. {@link https://grandstack.io/docs/neo4j-graphql-js|See this guide} for more information.
+     *
+     * @param {GraphQLSchema} schema
+     * @param {AugmentSchemaConfig} config
+     */
     export function augmentSchema(schema: GraphQLSchema, config: AugmentSchemaConfig): GraphQLSchema;
+
+    /**
+     * cypherQuery
+     * @description Generates a Cypher query (and associated parameters) to resolve a given GraphQL request (for a Query). Use this function when you want to handle the database call yourself, use neo4jgraphql for automated database call support.
+     *
+     * @param {RequestArguments} args
+     * @param {object} context
+     * @param {GraphQLResolveInfo} resolveInfo
+     */
+
     export function cypherQuery(args: RequestArguments, context: any, resolveInfo: GraphQLResolveInfo): CypherResult;
+
+    /**
+     * cypherMutation
+     * @description Similar to `cypherQuery`, but for mutations. Generates a Cypher query (and associated parameters) to resolve a given GraphQL request (for a Mutation). Use this function when you want to handle the database call yourself, use neo4jgraphql for automated database call support.
+     *
+     * @param {RequestArguments} args
+     * @param {object} context
+     * @param {GraphQLResolveInfo} resolveInfo
+     */
     export function cypherMutation(args: RequestArguments, context: any, resolveInfo: GraphQLResolveInfo): CypherResult;
 
     /**
